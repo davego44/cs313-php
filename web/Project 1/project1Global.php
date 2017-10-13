efc5a14e84933913494b43892d131fd1e68d3819
David Lobaccaro <davego44@yahoo.com>
2017-10-12 20:05:18 -0600

    Uploading Project 1 Beginnings

---
+++ web/Project 1/project1Global.php
@@ -0,0 +1,21 @@
+<?php
+	$dbUrl = getenv('DATABASE_URL');
+
+	$dbopts = parse_url($dbUrl);
+
+	$dbHost = $dbopts["host"];
+	$dbPort = $dbopts["port"];
+	$dbUser = $dbopts["user"];
+	$dbPassword = $dbopts["pass"];
+	$dbName = ltrim($dbopts["path"],'/');
+
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);		
+
+	session_start();
+	
+	$user_check = $_SESSION['user'];
+   
+    if(!isset($_SESSION['user'])){
+       header("location:project1Login.php");
+    }
+?>
---
+++ web/Project 1/project1Main.php
@@ -0,0 +1,14 @@
+<?php
+	require("project1Global.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>David Banking Login</title>
+		<link rel="stylesheet" type="text/css" href="#">
+	</head>
+	<body>
+		<h1>David Banking</h1>
+		<h3>Welcome! Thank you for logging in!</h3>
+	</body>
+</html>
