c46d8516cf794dee788b7d3b589fce1c8af32df3
David Lobaccaro <davego44@yahoo.com>
2017-10-18 20:42:43 -0600

    Updated

--- web/Team Activities/week6/week6.php
+++ web/Team Activities/week6/week6.php
@@ -46,16 +46,16 @@
 		if (!empty($_POST["book"]) && !empty($_POST["chapter"]) && 
 			!empty($_POST["verse"])  && !empty($_POST["topic"]) &&
 			!empty($_POST["content"])) {
-				$st = $db->prepare("INSERT INTO teamact.scriptures (book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)");
-				$st->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
-				$id = $db->lastInsertId();
+				//$st = $db->prepare("INSERT INTO teamact.scriptures (book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)");
+				//$st->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
+				//$id = $db->lastInsertId();
 				var_dump($topic);
 				/*foreach ($topic as $item) {
 					
 					$st = $db->prepare("INSERT INTO teamact.scripture_topics (topic_id, scripture_id) VALUES ((SELECT id FROM teamact.topics WHERE name = $item), $id)");
 					$st->execute();
 				}*/
-				header("Location: week6_2.php");
+				//header("Location: week6_2.php");
 		}
 	}
 	
