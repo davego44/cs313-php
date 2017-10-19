d3a36fecb3fb3e5781600e9b4cd2a31a7e0e8de6
David Lobaccaro <davego44@yahoo.com>
2017-10-18 21:03:13 -0600

    Updated

--- web/Team Activities/week6/week6.php
+++ web/Team Activities/week6/week6.php
@@ -47,8 +47,8 @@
 				$st = $db->prepare("INSERT INTO teamact.scriptures (book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)");
 				$st->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
 				$id = $db->lastInsertId();
-				foreach ($_POST['topic'] as $key => $value) {
-					var_dump($value);
+				var_dump($id);
+				foreach($_POST['topic'] as $key => $value) {
 					//$st = $db->prepare("INSERT INTO teamact.scripture_topics (topic_id, scripture_id) VALUES ((SELECT id FROM teamact.topics WHERE name = $value), $id)");
 					//$st->execute();
 				}
